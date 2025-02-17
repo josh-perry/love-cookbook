9bd18d4a5bb159d27388a26eb204e84b283fea27
Josh Perry <joshperry@outlook.com>
2025-02-17 17:12:58 +0000

    Use html base path plugin to fix paths

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
+
 export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets"); 
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
     return {
         pathPrefix: "/love-cookbook",
