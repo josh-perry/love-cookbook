6bdbd3423212fe89c9d079c1590c202f84d960e1
Sheepolution <danielhaazen@hotmail.com>
2025-02-23 20:34:10 +0100

    Add code line highlights. Resolves #19

---
+++ assets/js/codeblock.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll("pre code").forEach(codeBlock => {
+        const pre = codeBlock.closest("pre");
+        const wrapper = document.createElement("div");
+        wrapper.className = "code-block";
+
+        const linesSpan = codeBlock.querySelector("span[data-attrs]");
+        let highlightLines = new Set();
+
+        if (linesSpan) {
+            const ranges = linesSpan.getAttribute("data-attrs").split(",");
+            ranges.forEach(range => {
+                const [start, end] = range.split("-").map(Number);
+                if (end) {
+                    for (let i = start; i <= end; i++) {
+                        highlightLines.add(i);
+                    }
+                } else {
+                    highlightLines.add(start);
+                }
+            });
+
+            linesSpan.remove();
+        }
+
+        const codeText = codeBlock.innerText;
+        const totalLines = codeText.split("\n").length;
+
+        const highlightDiv = document.createElement("div");
+        highlightDiv.className = "highlight-lines";
+
+        for (let i = 1; i <= totalLines; i++) {
+            const highlightLine = document.createElement("div");
+            highlightLine.className = "highlight-line";
+            if (highlightLines.has(i)) {
+                highlightLine.classList.add("active");
+            }
+
+            highlightDiv.appendChild(highlightLine);
+        }
+
+        wrapper.appendChild(pre.cloneNode(true));
+        wrapper.appendChild(highlightDiv);
+        pre.replaceWith(wrapper);
+    });
+});
